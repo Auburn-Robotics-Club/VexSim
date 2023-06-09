db7cef8691777bf22ecd5f51048d07faf101e623
Carson E <30803947+pydev101@users.noreply.github.com>
2023-06-09 13:09:30 -0500

    Added colors

--- graphing.cpp
+++ graphing.cpp
@@ -1,49 +1,51 @@
 #include "graphing.h"
 
-void matplotlibcpp::scatter(Point2d p) {
+double DOT_SIZE = 3;
+
+void matplotlibcpp::scatter(Point2d p, std::string color) {
 	std::vector<double> x, y;
 	x.push_back(p.x);
 	y.push_back(p.y);
-	matplotlibcpp::scatter<double, double>(x, y);
+	matplotlibcpp::scatter<double, double>(x, y, DOT_SIZE, { {"color", color} });
 };
 
-void matplotlibcpp::quiver(Point2d p, Vector2d v) {
+void matplotlibcpp::quiver(Point2d p, Vector2d v, std::string color) {
 	std::vector<double> x, y, a, b;
 	x.push_back(p.x);
 	y.push_back(p.y);
 	a.push_back(v.getX());
 	b.push_back(v.getY());
-	matplotlibcpp::quiver<double, double>(x, y, a, b);
-	matplotlibcpp::scatter<double, double>(x, y);
+	matplotlibcpp::quiver<double, double>(x, y, a, b, { {"color", color} });
+	matplotlibcpp::scatter<double, double>(x, y, DOT_SIZE, { {"color", color} });
 };
 
-void matplotlibcpp::scatter(positionSet p) {
+void matplotlibcpp::scatter(positionSet p, std::string color) {
 	matplotlibcpp::scatter(p.p);
 };
 
-void matplotlibcpp::quiver(positionSet p, double mag) {
+void matplotlibcpp::quiver(positionSet p, double mag, std::string color) {
 	matplotlibcpp::quiver(p.p, Vector2d(mag, p.head, false));
 };
 
-void matplotlibcpp::plot(std::vector<Point2d>& p) {
+void matplotlibcpp::plot(std::vector<Point2d>& p, std::string color) {
 	std::vector<double> x, y;
 	for (int i = 0; i < p.size(); i++) {
 		x.push_back(p[i].x);
 		y.push_back(p[i].y);
 	}
-	matplotlibcpp::plot<double, double>(x, y);
+	matplotlibcpp::plot<double>(x, y, { {"color", color} });
 };
 
-void matplotlibcpp::scatter(std::vector<Point2d>& p) {
+void matplotlibcpp::scatter(std::vector<Point2d>& p, std::string color) {
 	std::vector<double> x, y;
 	for (int i = 0; i < p.size(); i++) {
 		x.push_back(p[i].x);
 		y.push_back(p[i].y);
 	}
-	matplotlibcpp::scatter<double, double>(x, y);
+	matplotlibcpp::scatter<double, double>(x, y, DOT_SIZE, { {"color", color} });
 };
 
-void matplotlibcpp::quiver(std::vector <Point2d>& p, std::vector <Vector2d>& v) {
+void matplotlibcpp::quiver(std::vector <Point2d>& p, std::vector <Vector2d>& v, std::string color) {
 	std::vector<double> x, y, a, b;
 	for (int i = 0; i < p.size(); i++) {
 		x.push_back(p[i].x);
@@ -51,28 +53,28 @@ void matplotlibcpp::quiver(std::vector <Point2d>& p, std::vector <Vector2d>& v) 
 		a.push_back(v[i].getX());
 		b.push_back(v[i].getY());
 	}
-	matplotlibcpp::quiver<double, double>(x, y, a, b);
+	matplotlibcpp::quiver<double, double>(x, y, a, b, { {"color", color} });
 };
 
-void matplotlibcpp::plot(std::vector <positionSet>& p) {
+void matplotlibcpp::plot(std::vector <positionSet>& p, std::string color) {
 	std::vector<double> x, y;
 	for (int i = 0; i < p.size(); i++) {
 		x.push_back(p[i].p.x);
 		y.push_back(p[i].p.y);
 	}
-	matplotlibcpp::plot<double, double>(x, y);
+	matplotlibcpp::plot<double>(x, y, { {"color", color} });
 };
 
-void matplotlibcpp::scatter(std::vector <positionSet>& p) {
+void matplotlibcpp::scatter(std::vector <positionSet>& p, std::string color) {
 	std::vector<double> x, y;
 	for (int i = 0; i < p.size(); i++) {
 		x.push_back(p[i].p.x);
 		y.push_back(p[i].p.y);
 	}
-	matplotlibcpp::scatter<double, double>(x, y);
+	matplotlibcpp::scatter<double, double>(x, y, DOT_SIZE, { {"color", color} });
 };
 
-void matplotlibcpp::quiver(std::vector <positionSet>& p, double mag) {
+void matplotlibcpp::quiver(std::vector <positionSet>& p, double mag, std::string color) {
 	std::vector<double> x, y, a, b;
 	for (int i = 0; i < p.size(); i++) {
 		x.push_back(p[i].p.x);
@@ -81,17 +83,17 @@ void matplotlibcpp::quiver(std::vector <positionSet>& p, double mag) {
 		a.push_back(v.getX());
 		b.push_back(v.getY());
 	}
-	matplotlibcpp::quiver<double, double>(x, y, a, b);
+	matplotlibcpp::quiver<double, double>(x, y, a, b, {{"color", color}});
 };
 
-void matplotlibcpp::plot(Path& path) {
-	matplotlibcpp::plot(path.getList());
+void matplotlibcpp::plot(Path& path, std::string color) {
+	matplotlibcpp::plot(path.getList(), color);
 };
-void matplotlibcpp::scatter(Path& path) {
-	matplotlibcpp::scatter(path.getList());
+void matplotlibcpp::scatter(Path& path, std::string color) {
+	matplotlibcpp::scatter(path.getList(), color);
 };
-void matplotlibcpp::quiver(Path& path, double mag) {
-	matplotlibcpp::quiver(path.getList(), mag);
+void matplotlibcpp::quiver(Path& path, double mag, std::string color) {
+	matplotlibcpp::quiver(path.getList(), mag, color);
 };
 
 void drawField() {
--- graphing.h
+++ graphing.h
@@ -4,23 +4,25 @@
 #include "AUBIE-VEX-Core/robotmath.h"
 #include "AUBIE-VEX-Core/navigation.h"
 
+extern double DOT_SIZE;
+
 namespace matplotlibcpp {
-	void scatter(Point2d p);
-	void quiver(Point2d p, Vector2d v);
-	void scatter(positionSet p);
-	void quiver(positionSet p, double mag = 1);
+	void scatter(Point2d p, std::string color = "black");
+	void quiver(Point2d p, Vector2d v, std::string color = "black");
+	void scatter(positionSet p, std::string color = "black");
+	void quiver(positionSet p, double mag = 1, std::string color = "black");
 
-	void plot(std::vector<Point2d>& p);
-	void scatter(std::vector<Point2d>& p);
-	void quiver(std::vector <Point2d>& p, std::vector <Vector2d>& v);
+	void plot(std::vector<Point2d>& p, std::string color = "black");
+	void scatter(std::vector<Point2d>& p, std::string color = "black");
+	void quiver(std::vector <Point2d>& p, std::vector <Vector2d>& v, std::string color = "black");
 
-	void plot(std::vector <positionSet>& p);
-	void scatter(std::vector <positionSet>& p);
-	void quiver(std::vector <positionSet>& p, double mag = 1);
+	void plot(std::vector <positionSet>& p, std::string color = "black");
+	void scatter(std::vector <positionSet>& p, std::string color = "black");
+	void quiver(std::vector <positionSet>& p, double mag = 1, std::string color = "black");
 
-	void plot(Path& path);
-	void scatter(Path& path);
-	void quiver(Path& path, double mag = 1);
+	void plot(Path& path, std::string color = "black");
+	void scatter(Path& path, std::string color = "black");
+	void quiver(Path& path, double mag = 1, std::string color = "black");
 
 	void drawField();
 }
