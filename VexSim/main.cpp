da49af478fd3581d40701f9066f570dab1cd25b6
Carson E <30803947+pydev101@users.noreply.github.com>
2023-06-13 22:07:21 -0500

    FInished graphing tool

--- VexSim/main.cpp
+++ VexSim/main.cpp
@@ -3,7 +3,18 @@
 #include "navigation.h"
 #include <chrono>
 
-startingPosition startPos = { Point2d(72, 2), 45, true };
+//Graph under robot
+//Graph over robot
+//Include color options
+//Quiver is rectangle with convex shape (triangle) at end; Define a fucntion to cast Vector2d to Vector2f
+//Graph onto texture that is drawn, when new things get added they get added to the texture, clearing the graph clears the texure
+//Use drawing shapes
+
+//TODO Add noise to GPS/Inertial add sliding to wheels using phycics in given github
+
+//In robotmath define operations for std::vector<Point2d and Vector2d> that allows you to add a Vector, rotate, or scale the entire list
+
+startingPosition startPos = { Point2d(72, 72), 90, true };
 const double tankDriveWidth = 16; //Inches
 const double robotWidth = 18;
 const double robotLength = 18;
@@ -22,14 +33,38 @@ int main() {
     realRobot.add(&horE);
     realRobot.add(&inertialSensor);
 
+    rightM.setVelocity(0);
+    leftM.setVelocity(0);
+
+    //Navigation Setup
+    TrackingBase<vex::motor, vex::motor, vex::encoder> trackBase
+        = TrackingBase<vex::motor, vex::motor, vex::encoder>(&leftM, false, &rightM, false, tankDriveWidth, &inertialSensor, &horE, false);
+    trackBase.setGlobalCoefficent(M_2PI * wheelRadius);
+    trackBase.setHeading(startPos.currentHeading);
+    Point2d trackingBasePos = startPos.currentPosition; //For debugging will be replaced with proper navigation class
+
     //Visuals
     simulator::Field field(floor(simulator::WINDOW_HEIGHT * 1), "Field2023.png");
     simulator::RobotSprite robotSprite(&realRobot, "tank.png", field);
+    simulator::FieldGraph graphBack(field);
+    simulator::FieldGraph graphFront(field);
 
-    int t = 0;
+        //Tracking Bot Rect
+            sf::RenderTexture trackedBotLocationTexture;
+            trackedBotLocationTexture.create(robotSprite.getGlobalBounds().width, robotSprite.getGlobalBounds().height);
+            sf::RectangleShape trackedBotRectShape(sf::Vector2f(robotSprite.getGlobalBounds().width, robotSprite.getGlobalBounds().height));
+            trackedBotRectShape.setFillColor(simulator::colors::GREEN);
+            trackedBotLocationTexture.draw(trackedBotRectShape);
+
+            sf::Sprite trackedBot;
+            trackedBot.setTexture(trackedBotLocationTexture.getTexture());
+            trackedBot.setOrigin((sf::Vector2f)trackedBotLocationTexture.getSize() / 2.f);
+
     double accel = 40.0 / 3000;
+    int t = 0;
 
     auto tLast = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = tLast - tLast;
     while (simulator::window.isOpen()) {
         //Events
         sf::Event event;
@@ -40,24 +75,40 @@ int main() {
         }
 
         //Robot Update
-        rightM.setVelocity(100 - accel * t);
-        leftM.setVelocity(58 + accel * t);
+        if (duration.count() > WAIT_TIME / 1000.0) {
+            trackBase.update();
+            trackingBasePos = trackBase.getAbsVector() + trackingBasePos;
+
+            rightM.setVelocity(20 + accel *t);
+            leftM.setVelocity(40 - accel * t);
+
+            //Sprite Update
+            graphFront.clear();
+            graphFront.scatter(trackingBasePos.x, trackingBasePos.y);
+            trackedBot.setPosition(sf::Vector2f(trackingBasePos.x * field.sprite.getGlobalBounds().width / field.WIDTH_INCHES, field.sprite.getGlobalBounds().height - trackingBasePos.y * field.sprite.getGlobalBounds().height / field.HEIGHT_INCHES));
+            trackedBot.setRotation(90 - radToDeg(normalizeAngle(trackBase.getHeading()))); //CW in deg
+        }
+
+        graphBack.plot3({ 72, 74, 2 }, { 72, 79, 85 }, {1, 2, 3});
+
 
         //Sprite Updates
-        robotSprite.updateSprite();
-
         simulator::window.clear();
         simulator::window.draw(field.sprite);
+        simulator::window.draw(graphBack);
+        simulator::window.draw(trackedBot);
         simulator::window.draw(robotSprite);
+        simulator::window.draw(graphFront);
         simulator::window.display();
 
-        auto tNow = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> duration = tNow - tLast;
 
         //Phycics Update
+        auto tNow = std::chrono::high_resolution_clock::now();
+        duration = tNow - tLast;
+        std::cout << duration.count() << std::endl;
         if (duration.count() > WAIT_TIME / 1000.0) {
-            //std::cout << realRobot.getCenter() << ", " << realRobot.getHeading() << std::endl;
             realRobot.update(WAIT_TIME);
+            robotSprite.updateSprite();
             tLast = tNow;
             t += WAIT_TIME;
         }
